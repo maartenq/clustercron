3a1512af88fd6aa1efa40d7312c458fb706afcd6
Maarten <@twaarten>
2016-08-28 14:34:45 +0200

    Bump version: 0.4.8 → 0.4.9

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='clustercron',
-    version='0.4.8',
+    version='0.4.9',
     description='Cron job wrapper that ensures a script gets run from one node'
     ' in the cluster.',
     long_description=readme + '\n\n' + history,
