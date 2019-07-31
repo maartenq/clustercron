8e9480f8eb73ba2affaa3989eb51f10be9c720d8
Maarten <ikmaarten@gmail.com>
2019-07-31 16:15:48 +0200

    Bump version: 0.4.10 → 0.5.0

--- clustercron/__init__.py
+++ clustercron/__init__.py
@@ -3,4 +3,4 @@
 
 __author__ = 'Maarten'
 __email__ = 'ikmaarten@gmail.com'
-__version__ = '0.4.10'
+__version__ = '0.5.0'
--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='clustercron',
-    version='0.4.10',
+    version='0.5.0',
     description='Cron job wrapper that ensures a script gets run from one node'
     ' in the cluster.',
     long_description=readme + '\n\n' + history,
